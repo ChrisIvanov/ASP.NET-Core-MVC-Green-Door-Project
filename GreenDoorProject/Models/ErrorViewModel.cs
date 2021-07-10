7ffa47c7d542cb37f38cbc3918d93099cdbcfd04
Chris Ivanov <kristian.ivanov92@gmail.com>
2021-07-10 10:14:46 +0300

    Created ASP.NET Core MVC project from scratch with Authentication and Razor runtime compilation.

---
+++ GreenDoorProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenDoorProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
